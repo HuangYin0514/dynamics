196e97728232e501260d4a60106cced8247195a7
HuangYin <1071324110@qq.com>
2022-01-15 10:55:12 +0800

    fix

--- 1D_Burgers_Equation/PINNFramework/PINN.py
+++ 1D_Burgers_Equation/PINNFramework/PINN.py
@@ -758,6 +758,7 @@ class PINN(nn.Module):
                         ),
                         flush=True,
                     )
+                    print(123)
 
                 if logger is not None and not (epoch + 1) % writing_cycle:
                     logger.log_scalar(
