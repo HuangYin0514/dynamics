3c140cd80fa1697c5c67fb80eda014bb0be5f9b2
HuangYin <1071324110@qq.com>
2022-01-17 04:39:16 +0800

    fix

--- Aupported_Beam_Equation/1D_Burgers_Equation.py
+++ Aupported_Beam_Equation/1D_Burgers_Equation.py
@@ -26,11 +26,10 @@ if __name__ == "__main__":
     N_f = 10000
     layers = [2, 20, 20, 20, 20, 20, 20, 20, 20, 1]
 
-    data = scipy.io.loadmat("Aupported_Beam_Equation/data/burgers_shock.mat")
+    data = scipy.io.loadmat("1D_Burgers_Equation_custom/data/burgers_shock.mat")
 
-    t = np.linspace(0, 1, 100).flatten()[:, None]
-    x = np.linspace(0, 1, 256).flatten()[:, None]
-    # Exact = np.zeros([100,256])
+    t = data["t"].flatten()[:, None]
+    x = data["x"].flatten()[:, None]
     Exact = np.real(data["usol"]).T
 
     X, T = np.meshgrid(x, t)
@@ -39,16 +38,16 @@ if __name__ == "__main__":
     u_star = Exact.flatten()[:, None]
 
     # Doman bounds
-    lb = np.array([0.0, 0.0])
-    ub = np.array([1.0, 0.99])
+    lb = X_star.min(0)
+    ub = X_star.max(0)
 
     # 左 上 下 边界
-    xx1 = np.hstack((X[0:1, :].T, T[0:1, :].T))  # 左
-    uu1 = np.zeros([256, 1])
-    xx2 = np.hstack((X[:, 0:1], T[:, 0:1]))  # 下
-    uu2 = np.zeros([100, 1])
-    xx3 = np.hstack((X[:, -1:], T[:, -1:]))  # 上
-    uu3 = np.zeros([100, 1])
+    xx1 = np.hstack((X[0:1, :].T, T[0:1, :].T))
+    uu1 = Exact[0:1, :].T
+    xx2 = np.hstack((X[:, 0:1], T[:, 0:1]))
+    uu2 = Exact[:, 0:1]
+    xx3 = np.hstack((X[:, -1:], T[:, -1:]))
+    uu3 = Exact[:, -1:]
 
     X_u_train = np.vstack([xx1, xx2, xx3])
     X_f_train = lb + (ub - lb) * lhs(2, N_f)
@@ -59,39 +58,15 @@ if __name__ == "__main__":
     X_u_train = X_u_train[idx, :]
     u_train = u_train[idx, :]
 
-    X_u_derivative_xx_trian = np.vstack([xx2, xx3])
-    u_derivative_xx_trian = np.vstack([uu2, uu3])
-    idx = np.random.choice(
-        X_u_derivative_xx_trian.shape[0], int(N_u / 2), replace=False
-    )
-    X_u_derivative_xx_trian = X_u_derivative_xx_trian[idx, :]
-    u_derivative_xx_trian = u_derivative_xx_trian[idx, :]
-
-    idx = np.random.choice(xx1.shape[0], int(N_u / 2), replace=False)
-    X_u_derivative_t_trian = xx1[idx, :]
-    u_derivative_t_trian = xx1[idx, :]
-
-    model = PhysicsInformedNN(
-        X_u_train,
-        u_train,
-        X_u_derivative_xx_trian,
-        u_derivative_xx_trian,
-        X_u_derivative_t_trian,
-        u_derivative_t_trian,
-        X_f_train,
-        layers,
-        lb,
-        ub,
-        nu,
-    )
+    model = PhysicsInformedNN(X_u_train, u_train, X_f_train, layers, lb, ub, nu)
     model.train()
 
     u_pred, f_pred = model.predict(X_star)
-    # error_u = np.linalg.norm(u_star - u_pred, 2) / np.linalg.norm(u_star, 2)
-    # print("Relatively Error u: %e" % (error_u))
+    error_u = np.linalg.norm(u_star - u_pred, 2) / np.linalg.norm(u_star, 2)
+    print("Relatively Error u: %e" % (error_u))
     U_pred = griddata(X_star, u_pred.flatten(), (X, T), method="cubic")
-    # Error = np.abs(Exact - U_pred)
-    # print("Absolute Error u: {}".format(np.sum(Error)))
+    Error = np.abs(Exact - U_pred)
+    print("Absolute Error u: {}".format(np.sum(Error)))
 
     ####### Row 0: u(t,x) ##################
     fig = plt.figure(figsize=(9, 5))
@@ -158,14 +133,14 @@ if __name__ == "__main__":
     )
 
     ax = plt.subplot(gs1[0, 0])
-    # ax.plot(x, Exact[25, :], "b-", linewidth=2, label="Exact")
+    ax.plot(x, Exact[25, :], "b-", linewidth=2, label="Exact")
     ax.plot(x, U_pred[25, :], "r--", linewidth=2, label="Prediction")
     ax.set_xlabel("$x$")
     ax.set_ylabel("$u(t,x)$")
     ax.set_title("$t = 0.25$", fontsize=15)
     ax.axis("square")
-    ax.set_xlim([-0.1, 1.1])
-    ax.set_ylim([-0.1, 1.1])
+    ax.set_xlim([-1.1, 1.1])
+    ax.set_ylim([-1.1, 1.1])
 
     for item in (
         [ax.title, ax.xaxis.label, ax.yaxis.label]
@@ -175,13 +150,13 @@ if __name__ == "__main__":
         item.set_fontsize(15)
 
     ax = plt.subplot(gs1[0, 1])
-    # ax.plot(x, Exact[50, :], "b-", linewidth=2, label="Exact")
+    ax.plot(x, Exact[50, :], "b-", linewidth=2, label="Exact")
     ax.plot(x, U_pred[50, :], "r--", linewidth=2, label="Prediction")
     ax.set_xlabel("$x$")
     ax.set_ylabel("$u(t,x)$")
     ax.axis("square")
-    ax.set_xlim([-0.1, 1.1])
-    ax.set_ylim([-0.1, 1.1])
+    ax.set_xlim([-1.1, 1.1])
+    ax.set_ylim([-1.1, 1.1])
     ax.set_title("$t = 0.50$", fontsize=15)
     ax.legend(
         loc="upper center",
@@ -199,13 +174,13 @@ if __name__ == "__main__":
         item.set_fontsize(15)
 
     ax = plt.subplot(gs1[0, 2])
-    # ax.plot(x, Exact[75, :], "b-", linewidth=2, label="Exact")
+    ax.plot(x, Exact[75, :], "b-", linewidth=2, label="Exact")
     ax.plot(x, U_pred[75, :], "r--", linewidth=2, label="Prediction")
     ax.set_xlabel("$x$")
     ax.set_ylabel("$u(t,x)$")
     ax.axis("square")
-    ax.set_xlim([-0.1, 1.1])
-    ax.set_ylim([-0.1, 1.1])
+    ax.set_xlim([-1.1, 1.1])
+    ax.set_ylim([-1.1, 1.1])
     ax.set_title("$t = 0.75$", fontsize=15)
 
     for item in (
@@ -216,33 +191,33 @@ if __name__ == "__main__":
         item.set_fontsize(15)
     plt.savefig("item.png")
 
-    # ####### ERROR for model ##################
-    # fig = plt.figure(figsize=(9, 5))
-    # ax = fig.add_subplot(111)
+    ####### ERROR for model ##################
+    fig = plt.figure(figsize=(9, 5))
+    ax = fig.add_subplot(111)
 
-    # h = ax.imshow(
-    #     np.abs((U_pred - Exact).T),
-    #     interpolation="nearest",
-    #     cmap="rainbow",
-    #     extent=[t.min(), t.max(), x.min(), x.max()],
-    #     origin="lower",
-    #     aspect="auto",
-    # )
-    # divider = make_axes_locatable(ax)
-    # cax = divider.append_axes("right", size="5%", pad=0.10)
-    # cbar = fig.colorbar(h, cax=cax)
-    # cbar.ax.tick_params(labelsize=15)
+    h = ax.imshow(
+        np.abs((U_pred - Exact).T),
+        interpolation="nearest",
+        cmap="rainbow",
+        extent=[t.min(), t.max(), x.min(), x.max()],
+        origin="lower",
+        aspect="auto",
+    )
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.10)
+    cbar = fig.colorbar(h, cax=cax)
+    cbar.ax.tick_params(labelsize=15)
 
-    # ax.set_xlabel("$t$", size=20)
-    # ax.set_ylabel("$x$", size=20)
-    # ax.legend(
-    #     loc="upper center",
-    #     bbox_to_anchor=(0.9, -0.05),
-    #     ncol=5,
-    #     frameon=False,
-    #     prop={"size": 15},
-    # )
-    # ax.set_title("$u(t,x)$", fontsize=20)  # font size doubled
-    # ax.tick_params(labelsize=15)
+    ax.set_xlabel("$t$", size=20)
+    ax.set_ylabel("$x$", size=20)
+    ax.legend(
+        loc="upper center",
+        bbox_to_anchor=(0.9, -0.05),
+        ncol=5,
+        frameon=False,
+        prop={"size": 15},
+    )
+    ax.set_title("$u(t,x)$", fontsize=20)  # font size doubled
+    ax.tick_params(labelsize=15)
 
-    # plt.savefig("error.png")
+    plt.savefig("error.png")
--- Aupported_Beam_Equation/model.py
+++ Aupported_Beam_Equation/model.py
@@ -1,3 +1,4 @@
+from re import L
 import torch
 from collections import OrderedDict
 import numpy as np
@@ -42,7 +43,20 @@ class DNN(torch.nn.Module):
 
 # the physics-guided neural network
 class PhysicsInformedNN:
-    def __init__(self, X_u, u, X_f, layers, lb, ub, nu):
+    def __init__(
+        self,
+        X_u,
+        u,
+        X_u_derivative_xx,
+        u_derivative_xx,
+        X_u_derivative_t,
+        u_derivative_t,
+        X_f,
+        layers,
+        lb,
+        ub,
+        nu,
+    ):
 
         # boundary conditions
         self.lb = torch.tensor(lb).float().to(device)
@@ -54,9 +68,27 @@ class PhysicsInformedNN:
         self.x_f = torch.tensor(X_f[:, 0:1], requires_grad=True).float().to(device)
         self.t_f = torch.tensor(X_f[:, 1:2], requires_grad=True).float().to(device)
         self.u = torch.tensor(u).float().to(device)
-
+        
+        self.x_u_derivative_xx = torch.tensor(X_u_derivative_xx[:, 0:1], requires_grad=True).float().to(device)
+        self.t_u_derivative_xx = torch.tensor(X_u_derivative_xx[:, 1:2], requires_grad=True).float().to(device)
+        self.u_derivative_xx = torch.tensor(u_derivative_xx).float().to(device)
+        
+        self.x_u_derivative_t = torch.tensor(X_u_derivative_t[:, 0:1], requires_grad=True).float().to(device)
+        self.t_u_derivative_t = torch.tensor(X_u_derivative_t[:, 1:2], requires_grad=True).float().to(device)
+        self.u_derivative_t = torch.tensor(u_derivative_t).float().to(device)
+        
         self.layers = layers
         self.nu = nu
+
+        Rho = 1000
+        E = 1.5e8
+        I = (2e-6) / 3
+        P = 1000
+        v = -0.15
+        self.pde_param1 = Rho
+        self.pde_param2 = 2 * Rho * v
+        self.pde_param3 = P - Rho * v ** 2
+        self.pde_param4 = E * I
 
         # deep neural networks
         self.dnn = DNN(layers).to(device)
@@ -90,6 +122,35 @@ class PhysicsInformedNN:
         u = self.dnn(torch.cat([x, t], dim=1))
         return u
 
+    def net_u(self, x, t):
+        u = self.dnn(torch.cat([x, t], dim=1))
+        return u
+
+    def net_u_t(self, x, t):
+        u = self.dnn(torch.cat([x, t], dim=1))
+
+        u_t = torch.autograd.grad(
+            u, t, grad_outputs=torch.ones_like(u), retain_graph=True, create_graph=True
+        )[0]
+
+        return u_t
+
+    def net_u_xx(self, x, t):
+        u = self.dnn(torch.cat([x, t], dim=1))
+
+        u_x = torch.autograd.grad(
+            u, x, grad_outputs=torch.ones_like(u), retain_graph=True, create_graph=True
+        )[0]
+        u_xx = torch.autograd.grad(
+            u,
+            x,
+            grad_outputs=torch.ones_like(u_x),
+            retain_graph=True,
+            create_graph=True,
+        )[0]
+
+        return u_xx
+
     def net_f(self, x, t):
         """The pytorch autograd version of calculating residual"""
         u = self.net_u(x, t)
@@ -100,6 +161,23 @@ class PhysicsInformedNN:
         u_x = torch.autograd.grad(
             u, x, grad_outputs=torch.ones_like(u), retain_graph=True, create_graph=True
         )[0]
+
+        u_tt = torch.autograd.grad(
+            u_t,
+            t,
+            grad_outputs=torch.ones_like(u_t),
+            retain_graph=True,
+            create_graph=True,
+        )[0]
+
+        u_xt = torch.autograd.grad(
+            u_x,
+            t,
+            grad_outputs=torch.ones_like(u_x),
+            retain_graph=True,
+            create_graph=True,
+        )[0]
+
         u_xx = torch.autograd.grad(
             u_x,
             x,
@@ -108,7 +186,28 @@ class PhysicsInformedNN:
             create_graph=True,
         )[0]
 
-        f = u_t + u * u_x - self.nu * u_xx
+        u_xxx = torch.autograd.grad(
+            u_xx,
+            x,
+            grad_outputs=torch.ones_like(u_xx),
+            retain_graph=True,
+            create_graph=True,
+        )[0]
+
+        u_xxxx = torch.autograd.grad(
+            u_xxx,
+            x,
+            grad_outputs=torch.ones_like(u_xxx),
+            retain_graph=True,
+            create_graph=True,
+        )[0]
+
+        f = (
+            self.pde_param1 * u_tt
+            + self.pde_param2 * u_xt
+            - self.pde_param3 * u_xx
+            + self.pde_param4 * u_xxxx
+        )
         return f
 
     def loss_func(self):
@@ -116,10 +215,16 @@ class PhysicsInformedNN:
 
         u_pred = self.net_u(self.x_u, self.t_u)
         f_pred = self.net_f(self.x_f, self.t_f)
+        f_pred = self.net_f(self.x_f, self.t_f)
         loss_u = torch.mean((self.u - u_pred) ** 2)
         loss_f = torch.mean(f_pred ** 2)
+        
+        u_xx_pred = self.net_u_xx(self.x_u_derivative_xx, self.t_u_derivative_xx)  # TODO
+        u_t_pred = self.net_u_t(self.x_u_derivative_t, self.t_u_derivative_t)  # TODO
+        loss_u_xx = torch.mean((self.u_derivative_xx - u_xx_pred) ** 2)
+        loss_u_t = torch.mean((self.u_derivative_t - u_t_pred) ** 2)
 
-        loss = loss_u + loss_f
+        loss = loss_u + loss_f + loss_u_xx + loss_u_t
 
         loss.backward()
         self.iter += 1
@@ -128,7 +233,6 @@ class PhysicsInformedNN:
                 "Iter %d, Loss: %.5e, Loss_u: %.5e, Loss_f: %.5e"
                 % (self.iter, loss.item(), loss_u.item(), loss_f.item())
             )
-            # print(self.optimizer.state_dict()["param_groups"][0]["lr"])
 
         return loss
 
