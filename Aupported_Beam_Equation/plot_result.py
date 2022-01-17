ea0808a7e36225ab9559791b9c65f9d695f9e326
HuangYin <1071324110@qq.com>
2022-01-17 13:16:45 +0800

    lr

--- Aupported_Beam_Equation/plot_result.py
+++ Aupported_Beam_Equation/plot_result.py
@@ -11,7 +11,7 @@ from model import *
 
 
 if __name__ == '__main__':
-    data = scipy.io.loadmat("pred-14.mat")
+    data = scipy.io.loadmat("pred-15.mat")
     u_pred = np.real(data["u_pred"])
     
     
