7f6f2f418d83e268dda6c0dd5bc924116fa810d0
HuangYin <1071324110@qq.com>
2022-01-25 09:25:24 +0800

    init pycrnet

--- Burgers_Equation_PhyCRNet/PhyCRNet/Datasets/Burgers_2d_solver_[HighOrder].py
+++ Burgers_Equation_PhyCRNet/PhyCRNet/Datasets/Burgers_2d_solver_[HighOrder].py
@@ -8,20 +8,6 @@ import matplotlib.pyplot as plt
 from random_fields import GaussianRF
 import torch
 
-
-try:
-    from torch import irfft
-    from torch import rfft
-except ImportError:
-    from torch.fft import irfft2
-    from torch.fft import rfft2
-    def rfft(x, d):
-        t = rfft2(x, dim = (-d))
-        return torch.stack((t.real, t.imag), -1)
-    def irfft(x, d, signal_sizes):
-        return irfft2(torch.complex(x[:,:,0], x[:,:,1]), s = signal_sizes, dim = (-d))
-
-
 torch.manual_seed(66)
 np.random.seed(66)
 
