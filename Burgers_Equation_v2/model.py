055d2703c62e1989e9fcd17d34c83adf30c35f24
HuangYin <1071324110@qq.com>
2022-02-01 22:38:53 +0800

    Burgers_Equation_v2

---
+++ Burgers_Equation_v2/model.py
@@ -0,0 +1,40 @@
+from collections import OrderedDict
+
+import torch
+
+
+# the deep neural network
+class DNN(torch.nn.Module):
+    def __init__(self, layers):
+        super(DNN, self).__init__()
+
+        # parameters
+        self.depth = len(layers) - 1
+
+        # set up layer order dict
+        self.activation = torch.nn.Tanh
+
+        layer_list = list()
+        for i in range(self.depth - 1):
+            layer_list.append(
+                ("layer_%d" % i, torch.nn.Linear(layers[i], layers[i + 1]))
+            )
+            layer_list.append(("activation_%d" % i, self.activation()))
+
+        layer_list.append(
+            ("layer_%d" % (self.depth - 1), torch.nn.Linear(layers[-2], layers[-1]))
+        )
+        layerDict = OrderedDict(layer_list)
+
+        # deploy layers
+        self.layers = torch.nn.Sequential(layerDict)
+
+    def forward(self, x):
+        out = self.layers(x)
+
+        return out
+
+
+if __name__ == '__main__':
+    net = DNN([2, 20, 20, 1])
+    print(net)
