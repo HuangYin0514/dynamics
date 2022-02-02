9dbb42f19459a7b63c7ae1ec33d7a3c451729ebf
HuangYin <1071324110@qq.com>
2022-02-02 21:19:38 +0800

    Burgers_Equation_v2 for dam

--- Burgers_Equation_v2/model.py
+++ Burgers_Equation_v2/model.py
@@ -13,11 +13,35 @@ class MlpBlock(nn.Module):
         return self.Tanh(self.fc(x))
 
 
+class DAM(nn.Module):
+    """ Discriminative Amplitude Modulator Layer (1-D) """
+
+    def __init__(self, in_dim):
+        super(DAM, self).__init__()
+        self.in_dim = in_dim
+
+        mu = torch.arange(self.in_dim).float() / self.in_dim * 5.0
+        self.mu = nn.Parameter(mu, requires_grad=False)
+        self.beta = nn.Parameter(torch.ones(1), requires_grad=True)
+        self.alpha = nn.Parameter(torch.ones(1), requires_grad=False)
+        self.register_parameter('mu', self.mu)
+        self.register_parameter('beta', self.beta)
+        self.register_parameter('alpha', self.alpha)
+        self.tanh = nn.Tanh()
+        self.sigmoid = nn.Sigmoid()
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        return x * self.mask()
+
+    def mask(self):
+        return self.relu(self.tanh((self.alpha ** 2) * (self.mu + self.beta)))
+
+
 class PINN(nn.Module):
     def __init__(self, num_blocks):
         super().__init__()
         self.num_blocks = num_blocks
-
 
         self.encoder = nn.Sequential(
             nn.Linear(2, 20),
@@ -25,6 +49,8 @@ class PINN(nn.Module):
         )
 
         self.mlp = self._make_layer(MlpBlock, num_blocks)
+
+        self.dam = DAM(in_dim=20)
 
         self.decoder = nn.Linear(20, 1)
 
@@ -40,6 +66,7 @@ class PINN(nn.Module):
     def forward(self, x):
         x = self.encoder(x)
         x = self.mlp(x)
+        x= self.dam(x)
         out = self.decoder(x)
         return out
 
@@ -52,6 +79,16 @@ if __name__ == '__main__':
     # print(net)
     # print(output.shape)
 
+    # test DAM
+    # net = DAM(2)
+    # t_input = torch.randn(6, 2)
+    # output = net(t_input)
+    # print(net)
+    # print(output.shape)
+    # print(t_input)
+    # print(output)
+
+    # test PINN
     net = PINN(8)
     t_input = torch.randn(32, 2)
     output = net(t_input)
---
+++ test_some_thing_for_python/t03.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+import  torch
+import torch.nn as nn
+if __name__ == "__main__":
+
+    in_dim =20
+
+    mu = torch.arange(in_dim).float() /in_dim * 5.0
+    mu = nn.Parameter(mu, requires_grad=False)
+    print()
