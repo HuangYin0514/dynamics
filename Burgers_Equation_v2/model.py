df344602d5d1725c18f16c726a3ec5a97f22a51a
HuangYin <1071324110@qq.com>
2022-02-04 02:00:53 +0800

    Burgers_Equation_v2 for model initialization

--- Burgers_Equation_v2/model.py
+++ Burgers_Equation_v2/model.py
@@ -1,6 +1,27 @@
 import torch
 from torch import nn
 
+def weights_init_kaiming(m):
+    classname = m.__class__.__name__
+    if classname.find("Linear") != -1:
+        nn.init.kaiming_normal_(m.weight, a=0, mode="fan_out")
+        nn.init.constant_(m.bias, 0.0)
+    elif classname.find("Conv") != -1:
+        nn.init.kaiming_normal_(m.weight, a=0, mode="fan_in")
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0.0)
+    elif classname.find("BatchNorm") != -1:
+        if m.affine:
+            nn.init.normal_(m.weight, 1.0, 0.02)
+            nn.init.constant_(m.bias, 0.0)
+
+
+def weights_init_classifier(m):
+    classname = m.__class__.__name__
+    if classname.find("Linear") != -1:
+        nn.init.normal_(m.weight, std=0.001)
+        if m.bias:
+            nn.init.constant_(m.bias, 0.0)
 
 class DAM(nn.Module):
     """ Discriminative Amplitude Modulator Layer (1-D) """
@@ -56,6 +77,10 @@ class PINN(nn.Module):
         self.dam = DAM(in_dim=20)
 
         self.decoder = nn.Linear(20, 1)
+
+        self.encoder.apply(weights_init_kaiming)
+        self.mlp.apply(weights_init_kaiming)
+        self.decoder.apply(weights_init_classifier)
 
     @staticmethod
     def _make_layer(block, num_blocks):
