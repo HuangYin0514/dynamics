480680f902927ed719994c2b477bf1082409b6bf
HuangYin <1071324110@qq.com>
2022-02-04 02:36:18 +0800

    Burgers_Equation_v2 for random

--- Burgers_Equation_v2/dataset2.py
+++ Burgers_Equation_v2/dataset2.py
@@ -18,34 +18,33 @@ class BurgersEquationDataSet(Dataset):
 
         n_u = 100
         n_f = 10000
-        data = scipy.io.loadmat('data/burgers_shock.mat')
 
-        t = data['t'].flatten()[:, None]
-        x = data['x'].flatten()[:, None]
-        Exact = np.real(data['usol']).T
+        # 上下界
+        self.lb = np.array([-1.0, 0.0])
+        self.ub = np.array([1.0, 1.0])  # (X,T)
 
-        X, T = np.meshgrid(x, t)
+        # 时间和空间点数
+        n_t = 100
+        n_x = 256
 
-        X_star = np.hstack((X.flatten()[:, None], T.flatten()[:, None]))
-        u_star = Exact.flatten()[:, None]
+        t = np.linspace(self.lb[1], self.ub[1], n_t).flatten()[:, None]
+        x = np.linspace(self.lb[0], self.ub[0], n_x).flatten()[:, None]
 
-        # Doman bounds
-        lb = X_star.min(0)
-        ub = X_star.max(0)
+        x_mesh, t_mesh = np.meshgrid(x, t)  # X(n_t,n_x) T(n_t,n_x)
 
-        xx1 = np.hstack((X[0:1, :].T, T[0:1, :].T))
-        uu1 = Exact[0:1, :].T
-        xx2 = np.hstack((X[:, 0:1], T[:, 0:1]))
-        uu2 = Exact[:, 0:1]
-        xx3 = np.hstack((X[:, -1:], T[:, -1:]))
-        uu3 = Exact[:, -1:]
+        xx1 = np.hstack((x_mesh[0:1, :].T, t_mesh[0:1, :].T))  # 左
+        uu1 = -1 * np.sin(xx1 * np.pi)[:, 0][:, None]
+        xx2 = np.hstack((x_mesh[:, 0:1], t_mesh[:, 0:1]))  # 下
+        uu2 = np.zeros([n_t, 1])
+        xx3 = np.hstack((x_mesh[:, -1:], t_mesh[:, -1:]))  # 上
+        uu3 = np.zeros([n_t, 1])
 
         # all bounds constraints
         x_u_train = np.vstack([xx1, xx2, xx3])
         u_train = np.vstack([uu1, uu2, uu3])
 
         # pde constraints
-        x_f_train = lb + (ub-lb)*lhs(2, n_f)
+        x_f_train = self.lb + (self.ub - self.lb) * lhs(2, n_f)
         self.x_f_train = np.vstack((x_f_train, x_u_train))
 
         # ib constraints
@@ -56,7 +55,7 @@ class BurgersEquationDataSet(Dataset):
         # Prediction
         data = scipy.io.loadmat("data/burgers_shock.mat")
         exact = np.real(data["usol"]).T
-        self.x_star = np.hstack((X.flatten()[:, None], T.flatten()[:, None]))  # (n_x*n_t, 2)
+        self.x_star = np.hstack((x_mesh.flatten()[:, None], t_mesh.flatten()[:, None]))  # (n_x*n_t, 2)
         self.u_star = exact.flatten()[:, None]
 
     def __len__(self):
