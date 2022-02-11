2ff256a492efef9e851441395592daee85518029
HuangYin <1071324110@qq.com>
2022-02-12 01:13:50 +0800

    deeponet for main

--- deeponet/ode/main.py
+++ deeponet/ode/main.py
@@ -1,5 +1,24 @@
+import random
+import torch.backends.cudnn
+import numpy as np
+import torch
+
 from deeponet.ode.dataset import IntegralData, DataGenerator
 from trainer import Trainer
+
+
+def init_random_state():
+    random_seed = 3407
+    torch.manual_seed(random_seed)
+    torch.cuda.manual_seed_all(random_seed)
+    torch.cuda.manual_seed(random_seed)
+    np.random.seed(random_seed)  # Numpy module.
+    random.seed(random_seed)  # Python random module.
+    torch.backends.cudnn.deterministic = True  # speed up compution
+    torch.backends.cudnn.benchmark = True
+
+
+init_random_state()
 
 if __name__ == '__main__':
     data = IntegralData()
