c5092689d089d9acabee9ea2fe981a4dc3ba5a0b
HuangYin <1071324110@qq.com>
2022-02-12 21:10:49 +0800

    deeponet ode for all adam

--- DeepONet_ode/utils.py
+++ DeepONet_ode/utils.py
@@ -36,5 +36,5 @@ def weights_init_xavier_normal(m):
     classname = m.__class__.__name__
     if classname.find("Linear") != -1:
         nn.init.xavier_normal_(m.weight)
-        if m.bias:
+        if m.bias is not None:
             nn.init.constant_(m.bias, 0.0)
