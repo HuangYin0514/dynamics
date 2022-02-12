08fbc2b36b7900c47684af247081b6c508a4e81f
HuangYin <1071324110@qq.com>
2022-02-12 21:07:12 +0800

    deeponet ode for all adam

--- DeepONet_ode/model.py
+++ DeepONet_ode/model.py
@@ -1,28 +1,7 @@
 import torch
 import torch.nn as nn
 
-
-def weights_init_kaiming(m):
-    classname = m.__class__.__name__
-    if classname.find("Linear") != -1:
-        nn.init.kaiming_normal_(m.weight, a=0, mode="fan_out")
-        nn.init.constant_(m.bias, 0.0)
-    elif classname.find("Conv") != -1:
-        nn.init.kaiming_normal_(m.weight, a=0, mode="fan_in")
-        if m.bias is not None:
-            nn.init.constant_(m.bias, 0.0)
-    elif classname.find("BatchNorm") != -1:
-        if m.affine:
-            nn.init.normal_(m.weight, 1.0, 0.02)
-            nn.init.constant_(m.bias, 0.0)
-
-
-def weights_init_classifier(m):
-    classname = m.__class__.__name__
-    if classname.find("Linear") != -1:
-        nn.init.normal_(m.weight, std=0.001)
-        if m.bias:
-            nn.init.constant_(m.bias, 0.0)
+from utils import weights_init_kaiming, weights_init_xavier_normal
 
 
 class MlpBlock(nn.Module):
@@ -95,7 +74,9 @@ class DeepONet(nn.Module):
         self.branch_net = BranchNet()
         self.trunk_net = TrunkNet()
         self.net_bias = nn.Parameter(torch.zeros([1]))
-        # self.branch_net.apply(weights_init_kaiming)
+
+        self.branch_net.apply(weights_init_xavier_normal)
+        self.trunk_net.apply(weights_init_xavier_normal)
 
     def forward(self, u, y):
         B = self.branch_net(u)
