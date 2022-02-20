49acb658625831e79713e7e3b0c28ae518fc44c5
HuangYin <1071324110@qq.com>
2022-02-21 01:38:26 +0800

    burgers for initial

--- DeepONet/burgers_v3_1/model.py
+++ DeepONet/burgers_v3_1/model.py
@@ -1,32 +1,7 @@
 import torch
 import torch.nn as nn
 
-from model_layers.mlp_mixer2 import MlpMixer
-
-
-class DAM(nn.Module):
-    """ Discriminative Amplitude Modulator Layer (1-D) """
-
-    def __init__(self, in_dim):
-        super(DAM, self).__init__()
-        self.in_dim = in_dim
-
-        mu = torch.arange(self.in_dim).float() / self.in_dim * 5.0
-        self.mu = nn.Parameter(mu, requires_grad=False)
-        self.beta = nn.Parameter(torch.ones(1), requires_grad=True)
-        self.alpha = nn.Parameter(torch.ones(1), requires_grad=False)
-        self.register_parameter('mu', self.mu)
-        self.register_parameter('beta', self.beta)
-        self.register_parameter('alpha', self.alpha)
-        self.tanh = nn.Tanh()
-        self.sigmoid = nn.Sigmoid()
-        self.relu = nn.ReLU()
-
-    def forward(self, x):
-        return x * self.mask()
-
-    def mask(self):
-        return self.relu(self.tanh((self.alpha ** 2) * (self.mu + self.beta)))
+from model_layers.mlp_mixer import MlpMixer
 
 
 class MlpBlock(nn.Module):
@@ -40,37 +15,6 @@ class MlpBlock(nn.Module):
 
     def forward(self, x):
         out = self.mlpBlock_layers(x)
-        return out
-
-
-class PINN(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.encoder = nn.Sequential(
-            nn.Linear(2, 40),
-            nn.Tanh()
-        )
-
-        self.mlp = self._make_layer(MlpBlock, num_blocks=7)
-
-        self.dam = DAM(in_dim=40)
-
-        self.decoder = nn.Linear(40, 40)
-
-    @staticmethod
-    def _make_layer(block, num_blocks):
-        layers = []
-        for _ in range(num_blocks):
-            layers.append(block(input_dim=40, output_dim=40))
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.encoder(x)
-        x = self.mlp(x)
-        x = self.dam(x)
-        out = self.decoder(x)
         return out
 
 
@@ -106,9 +50,9 @@ class DeepONet(nn.Module):
     def __init__(self):
         super().__init__()
         self.branch_net = BranchNet()
-        # self.trunk_net = MlpMixer(num_classes=40, num_blocks=10, patch_size=10, tokens_hidden_dim=32, channels_hidden_dim=40,
-        #                  tokens_mlp_dim=2, channels_mlp_dim=40)
-        self.trunk_net = MlpMixer()
+        self.trunk_net = MlpMixer(num_classes=40, num_blocks=10, patch_size=10, tokens_hidden_dim=32, channels_hidden_dim=40,
+                         tokens_mlp_dim=2, channels_mlp_dim=40)
+        # self.trunk_net = MlpMixer()
         self.net_bias = nn.Parameter(torch.zeros([1]))
 
         # self.branch_net.apply(weights_init_xavier_normal)
