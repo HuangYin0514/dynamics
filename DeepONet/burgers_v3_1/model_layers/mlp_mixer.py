b3a086bf7239bcb04bd7678dd01b33d4b0a4d8a9
HuangYin <1071324110@qq.com>
2022-02-21 20:35:34 +0800

    burgers for initial

--- DeepONet/burgers_v3_1/model_layers/mlp_mixer.py
+++ DeepONet/burgers_v3_1/model_layers/mlp_mixer.py
@@ -43,6 +43,7 @@ class T_MixerBlock(nn.Module):
 
         self.T_MixerBlock_layers = nn.Sequential(
             nn.Linear(1, 40),
+            nn.Tanh()
         )
 
     def forward(self, x):
@@ -55,6 +56,7 @@ class X_MixerBlock(nn.Module):
 
         self.X_MixerBlock_layers = nn.Sequential(
             nn.Linear(1, 40),
+            nn.Tanh()
         )
 
     def forward(self, x):
