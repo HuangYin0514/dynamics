8ab46f62ffab2b380bdd883a1426b5b97cc3bc60
HuangYin <1071324110@qq.com>
2022-02-25 19:33:49 +0800

    burgers for initial

--- LNN/ori_v1_1/DoublPendulum.py
+++ LNN/ori_v1_1/DoublPendulum.py
@@ -1,9 +1,9 @@
 from functools import partial  # reduces arguments to function by making some subset implicit
 
 import jax
-from data import get_trajectory_analytic
 
 from physics import analytical_fn
+from data import get_trajectory_analytic
 
 vfnc = jax.jit(jax.vmap(analytical_fn))
 vget = partial(jax.jit, backend='cpu')(jax.vmap(partial(get_trajectory_analytic, mxsteps=100), (0, None), 0))
--- LNN/ori_v1_1/__init__.py
+++
@@ -1,4 +0,0 @@
-from .lnn import lagrangian_eom, unconstrained_eom, solve_dynamics
-from .models import mlp, pixel_encoder, pixel_decoder
-from .plotting import get_dblpend_images, plot_dblpend
-from .utils import wrap_coords, rk4_step, write_to, read_from
