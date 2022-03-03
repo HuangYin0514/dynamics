2fe92b39eacfd9639f8eaf6fb5971d59613fbcc0
HuangYin <1071324110@qq.com>
2022-03-03 18:40:26 +0800

    LNN for doublepenmu

--- LNN/ori_v1_4/main.py
+++ LNN/ori_v1_4/main.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
 
     train_loader = torch.utils.data.DataLoader(
         doublePendulum_dataset,
-        batch_size=2000,
+        batch_size=256*500,
     )
 
     trainer = Trainer()
