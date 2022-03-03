a4db57dede6ffc27b74c8c658a71684cd2b805b0
HuangYin <1071324110@qq.com>
2022-03-03 09:33:58 +0800

    burgers for initial

--- LNN/ori_v1_1/DoublPendulum2.py
+++
@@ -1,8 +0,0 @@
-from functools import partial  # reduces arguments to function by making some subset implicit
-
-import jax
-
-from data import get_trajectory_analytic
-
-# vfnc = jax.jit(jax.vmap(analytical_fn))
-vget = partial(jax.jit, backend='cpu')(jax.vmap(partial(get_trajectory_analytic, mxsteps=100), (0, None), 0))
--- LNN/ori_v1_1/t02.py
+++
@@ -1,5 +0,0 @@
-from data import get_trajectory_analytic
-
-vfnc = jax.jit(jax.vmap(analytical_fn))
-vget = partial(jax.jit, backend='cpu')(jax.vmap(partial(get_trajectory_analytic, mxsteps=100), (0, None), 0))
-vget_unlimited = partial(jax.jit, backend='cpu')(jax.vmap(partial(get_trajectory_analytic), (0, None), 0))
