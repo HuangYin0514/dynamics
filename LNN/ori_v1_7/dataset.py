bc74e4a2cddbbdc936f3d9a97abf8f96ae047339
HuangYin <1071324110@qq.com>
2022-03-04 22:45:15 +0800

    LNN for doublepenmu

--- LNN/ori_v1_7/dataset.py
+++ LNN/ori_v1_7/dataset.py
@@ -23,8 +23,8 @@ class DoublePendulumData():
     def get_derivative_dataset(self):
 
         y0 = np.concatenate([
-            np.random.uniform(size=(3000*450, 2)) * 2.0 * np.pi,
-            np.random.uniform(size=(3000*450, 2)) * 0.1
+            np.random.uniform(size=(3000*340, 2)) * 2.0 * np.pi,
+            np.random.uniform(size=(3000*340, 2)) * 0.1
         ], axis=1)
 
         return y0 ,analytical_fn(y0)[:,2:4]
